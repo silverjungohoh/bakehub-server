405708500adbc8bc0194b159a6decb0206c662f3
grace <grace994084@gmail.com>
2023-08-25 17:39:44 +0900

    feat : 회원에 대한 예외 일괄적으로 처리

---
+++ src/main/java/com/project/snsserver/global/error/handler/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.project.snsserver.global.error.handler;
+
+import com.project.snsserver.global.error.exception.MemberException;
+import com.project.snsserver.global.error.model.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MemberException.class)
+    public ResponseEntity<ErrorResponse> handleMemberException(MemberException e) {
+        log.error("Error occurs {}", e.getMessage());
+
+        ErrorResponse response = ErrorResponse.builder()
+                .status(e.getErrorCode().getStatus().value())
+                .code(e.getErrorCode().getCode())
+                .message(e.getErrorCode().getMessage())
+                .build();
+
+        return ResponseEntity.status(e.getErrorCode().getStatus()).body(response);
+    }
+}
