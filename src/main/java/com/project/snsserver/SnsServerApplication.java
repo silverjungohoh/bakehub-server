d16ac8e50540f0a58e5233941cfa6c1572e24705
grace <grace994084@gmail.com>
2023-08-25 10:23:53 +0900

    initial commit

---
+++ src/main/java/com/project/snsserver/SnsServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.snsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnsServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/snsserver/SnsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.snsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
