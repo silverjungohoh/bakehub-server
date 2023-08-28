4c9269be1821ad00972c0fb8bee57638c03e166b
grace <grace994084@gmail.com>
2023-08-28 12:13:22 +0900

    feat : 유효성 검증에 대한 예외 일괄적으로 처리

--- src/main/java/com/project/snsserver/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/project/snsserver/global/error/handler/GlobalExceptionHandler.java
@@ -5,8 +5,15 @@ import com.project.snsserver.global.error.exception.MemberException;
 import com.project.snsserver.global.error.model.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.project.snsserver.global.error.type.ErrorCode.INVALID_ARGUMENT;
 
 @Slf4j
 @RestControllerAdvice
@@ -34,5 +41,22 @@ public class GlobalExceptionHandler {
                 .build();
 
         return ResponseEntity.status(e.getErrorCode().getStatus()).body(response);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse<Map<String,String>>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        Map<String, String> fieldError = new HashMap<>();
+        for(FieldError error : e.getFieldErrors()) {
+            fieldError.put(error.getField(), error.getDefaultMessage());
+        }
+
+        ErrorResponse<Map<String,String>> response = ErrorResponse.<Map<String,String>>builder()
+                .status(INVALID_ARGUMENT.getStatus().value())
+                .code(INVALID_ARGUMENT.getCode())
+                .message(fieldError)
+                .build();
+
+        return ResponseEntity.status(INVALID_ARGUMENT.getStatus()).body(response);
     }
 }
