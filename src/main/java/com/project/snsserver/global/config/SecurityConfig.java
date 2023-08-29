0902a354f89ce9c5fbdcf727053e0d8c2bc2c0a8
grace <grace994084@gmail.com>
2023-08-29 17:16:44 +0900

    feat : 권한 없음에 따른 예외 처리

--- src/main/java/com/project/snsserver/global/config/SecurityConfig.java
+++ src/main/java/com/project/snsserver/global/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.project.snsserver.global.config;
 
+import com.project.snsserver.domain.security.handler.CustomAccessDeniedHandler;
 import com.project.snsserver.domain.security.handler.CustomAuthenticationEntryPoint;
 import com.project.snsserver.domain.security.jwt.JwtAuthenticationFilter;
 import com.project.snsserver.domain.security.jwt.JwtExceptionFilter;
@@ -17,14 +18,17 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsUtils;
 
+import static com.project.snsserver.domain.member.type.MemberRole.ADMIN;
+
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final JwtExceptionFilter jwtExceptionFilter;
     private final CustomAuthenticationEntryPoint authenticationEntryPoint;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
 
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -54,10 +58,12 @@ public class SecurityConfig {
                 .antMatchers("/swagger-ui/**", "/h2-console/**", "/api-docs/**",
                         "/api/v1/members/sign-up", "/api/v1/members/auth/**",
                         "/api/v1/members/duplicate/**").permitAll()
+                .antMatchers("/api/v1/admin/**").hasAuthority(ADMIN.getValue())
                 .anyRequest().authenticated();
 
         http.exceptionHandling()
                 .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler)
 
                 .and()
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
