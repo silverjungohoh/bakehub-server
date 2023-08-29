bd2f2cca2a491846bc484e64d9662b7f8bf24c49
grace <grace994084@gmail.com>
2023-08-29 19:10:59 +0900

    feat : access token 재발급 요청 및 응답 dto 생성

---
+++ src/main/java/com/project/snsserver/domain/member/model/dto/ReissueTokenRequest.java
@@ -0,0 +1,15 @@
+package com.project.snsserver.domain.member.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReissueTokenRequest {
+
+    private String refreshToken;
+}
