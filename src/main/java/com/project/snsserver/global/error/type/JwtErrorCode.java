ac783f9f3489b423c5182fbe0277f39a16876496
grace <grace994084@gmail.com>
2023-08-29 13:07:26 +0900

    feat : jwt에 대한 사용자 정의 예외 및 에러 코드 생성

---
+++ src/main/java/com/project/snsserver/global/error/type/JwtErrorCode.java
@@ -0,0 +1,26 @@
+package com.project.snsserver.global.error.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+/**
+ * jwt 관련된 예외 CODE = E301 ~ E399
+ */
+
+@Getter
+@AllArgsConstructor
+public enum JwtErrorCode {
+
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "E301", "만료된 token 입니다."),
+
+    INVALID_TOKEN_SIGNATURE(HttpStatus.UNAUTHORIZED, "E302", "유효하지 않은 token 서명입니다."),
+
+    UNSUPPORTED_TOKEN(HttpStatus.UNAUTHORIZED, "E303", "지원되지 않는 token 입니다."),
+
+    INCORRECT_TOKEN(HttpStatus.UNAUTHORIZED, "E304", "잘못된 token 입니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
