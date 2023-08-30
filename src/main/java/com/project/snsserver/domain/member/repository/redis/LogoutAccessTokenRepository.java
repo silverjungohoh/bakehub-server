dc233b73fe9853a9994a9146edf428dc09ddf8a8
grace <grace994084@gmail.com>
2023-08-30 10:16:48 +0900

    feat : redis에 로그아웃 여부 저장하기 위한 객체 정의

---
+++ src/main/java/com/project/snsserver/domain/member/repository/redis/LogoutAccessTokenRepository.java
@@ -0,0 +1,7 @@
+package com.project.snsserver.domain.member.repository.redis;
+
+import com.project.snsserver.domain.member.model.entity.LogoutAccessToken;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LogoutAccessTokenRepository extends CrudRepository<LogoutAccessToken, String> {
+}
