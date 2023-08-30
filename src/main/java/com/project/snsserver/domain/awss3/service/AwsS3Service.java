daa1f9869d68dd1a3098c1626fcaa86d9a2eec92
grace <grace994084@gmail.com>
2023-08-30 14:42:19 +0900

    feat : aws s3에 복수의 이미지 업로드 구현

--- src/main/java/com/project/snsserver/domain/awss3/service/AwsS3Service.java
+++ src/main/java/com/project/snsserver/domain/awss3/service/AwsS3Service.java
@@ -2,12 +2,19 @@ package com.project.snsserver.domain.awss3.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 public interface AwsS3Service {
 
     /**
      * aws s3에 이미지 업로드
      */
     String uploadFile (MultipartFile file, String dir);
+
+    /**
+     * aws s3에 복수의 이미지 업로드
+     */
+    List<String> uploadFiles(List<MultipartFile> files, String dir);
 
     /**
      * aws s3에 업로드 된 이미지 삭제
--- src/main/java/com/project/snsserver/domain/awss3/service/AwsS3ServiceImpl.java
+++ src/main/java/com/project/snsserver/domain/awss3/service/AwsS3ServiceImpl.java
@@ -1,5 +1,6 @@
 package com.project.snsserver.domain.awss3.service;
 
+import com.amazonaws.AmazonClientException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
@@ -8,17 +9,20 @@ import com.project.snsserver.domain.awss3.type.FileExtension;
 import com.project.snsserver.global.error.exception.ImageException;
 import com.project.snsserver.global.error.type.ImageErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AwsS3ServiceImpl implements AwsS3Service {
@@ -45,11 +49,25 @@ public class AwsS3ServiceImpl implements AwsS3Service {
             amazonS3.putObject(new PutObjectRequest(bucket + "/" + dir, fileName, inputStream, objectMetadata)
                     .withCannedAcl(CannedAccessControlList.PublicRead));
 
-        } catch (IOException e) {
+        } catch (IOException  | AmazonClientException e) {
+            log.error("image upload fail : {}", e.getMessage());
             throw new ImageException(ImageErrorCode.FAIL_TO_UPLOAD_IMAGE);
         }
-
+        log.info("image upload success");
         return amazonS3.getUrl(bucket, dir + "/" + fileName).toString();
+    }
+
+    /**
+     * aws s3에 복수의 이미지 업로드
+     */
+    @Override
+    public List<String> uploadFiles(List<MultipartFile> files, String dir) {
+        List<String> imageUrls = new ArrayList<>();
+        files.forEach((file) -> {
+            String imageUrl = uploadFile(file, dir);
+            imageUrls.add(imageUrl);
+        });
+        return imageUrls;
     }
 
     /**
