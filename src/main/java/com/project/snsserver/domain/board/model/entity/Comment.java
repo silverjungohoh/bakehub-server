d338fc2def649775dec90358f4252b2962cb0ca0
grace <grace994084@gmail.com>
2023-08-31 16:04:24 +0900

    feat : 댓글 entity 생성

---
+++ src/main/java/com/project/snsserver/domain/board/model/entity/Comment.java
@@ -0,0 +1,30 @@
+package com.project.snsserver.domain.board.model.entity;
+
+import com.project.snsserver.domain.member.model.entity.Member;
+import com.project.snsserver.global.entity.BaseTimeEntity;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", updatable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", updatable = false)
+    private Post post;
+}
