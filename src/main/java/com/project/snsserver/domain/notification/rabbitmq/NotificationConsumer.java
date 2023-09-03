297d21bf3b3b86e483e2c01a2c3c1e5b4b20eef4
grace <grace994084@gmail.com>
2023-09-03 11:34:33 +0900

    feat : consumer 구현 (queue에서 메세지 받아옴)

---
+++ src/main/java/com/project/snsserver/domain/notification/rabbitmq/NotificationConsumer.java
@@ -0,0 +1,18 @@
+package com.project.snsserver.domain.notification.rabbitmq;
+
+import com.project.snsserver.domain.notification.model.dto.NotificationMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class NotificationConsumer {
+
+    @RabbitListener(queues = "notification.queue")
+    public void consumeNotification(NotificationMessage message) {
+        log.info(String.format("consume message >> %s", message));
+    }
+}
