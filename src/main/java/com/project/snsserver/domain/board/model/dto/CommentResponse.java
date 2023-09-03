f17f322a2ad931a59a57e20e554e76deb09ca458
grace <grace994084@gmail.com>
2023-09-03 14:51:30 +0900

    feat : 댓글 응답 dto 생성

---
+++ src/main/java/com/project/snsserver/domain/board/model/dto/CommentResponse.java
@@ -0,0 +1,33 @@
+package com.project.snsserver.domain.board.model.dto;
+
+import com.project.snsserver.domain.board.model.entity.Comment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentResponse {
+
+    private Long commentId;
+
+    private String content;
+
+    private String nickname;
+
+    private Timestamp createdAt;
+
+    public static CommentResponse fromEntity(Comment comment) {
+        return CommentResponse.builder()
+                .commentId(comment.getId())
+                .content(comment.getContent())
+                .nickname(comment.getMember().getNickname())
+                .createdAt(Timestamp.valueOf(comment.getCreatedAt()))
+                .build();
+    }
+}
