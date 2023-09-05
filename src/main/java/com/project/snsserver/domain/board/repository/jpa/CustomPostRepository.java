9849a9096a0faebfb4a0aa65c5f7ba143f143c04
grace <grace994084@gmail.com>
2023-09-05 23:37:11 +0900

    fix : 게시물 상세 조회 수정 (조회 쿼리 분리)

--- src/main/java/com/project/snsserver/domain/board/repository/jpa/CustomPostRepository.java
+++ src/main/java/com/project/snsserver/domain/board/repository/jpa/CustomPostRepository.java
@@ -1,6 +1,5 @@
 package com.project.snsserver.domain.board.repository.jpa;
 
-import com.project.snsserver.domain.board.model.dto.PostDetailResponse;
 import com.project.snsserver.domain.board.model.dto.PostResponse;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
@@ -10,7 +9,7 @@ public interface CustomPostRepository {
 
     Slice<PostResponse> findAllPostsWithCommentCntAndHeartCnt(Long lastPostId, Pageable pageable);
 
-    PostDetailResponse findPostDetailByPostId(Long postId);
+    PostResponse findPostByPostId(Long postId);
 
     Slice<PostResponse> findAllPostsByHashtag(Long lastPostId, String name, Pageable pageable);
 }
